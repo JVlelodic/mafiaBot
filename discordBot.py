b5cbaeb6ba932cc8ff31585f979fab066173878e
jzWang <jzwang2410@gmail.com>
2020-08-03 17:28:16 +1000

    converting to JS

--- discordBot.py
+++
@@ -1,8 +0,0 @@
-import discord
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Loggedon as {0}'.format(self.user))
-    
-    async def on_message(self, message):
-        print('')
