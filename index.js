a8a2ddaeb52de0b95b14733b3d370e8e7b625e4b
jzWang <jzwang2410@gmail.com>
2020-08-06 22:02:16 +1000

    added replies to bot

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-const Discord = require ("discord.js");
+const Discord = require("discord.js");
 const client = new Discord.Client();
+const config = require("./config.json");
+const TOKEN = config.token;
+
+client.on("ready", () => {
+    console.log("This bot is online");
+})
+
+client.on("message", msg => {
+    switch(msg.content){
+        case "HELLO":
+            msg.reply("Hello World");
+            break;    
+        case "speech":
+            msg.channel.send("Text to speech works!", { tts: true });
+            break;
+        default:
+    }
+})
+
+client.login(TOKEN);
+
 
 
 
