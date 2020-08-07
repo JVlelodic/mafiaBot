89238dfef29c166178bcc5dcea19b6a4559817b7
jzWang <jzwang2410@gmail.com>
2020-08-07 23:24:41 +1000

    updated formatting

--- index.js
+++ index.js
@@ -11,33 +11,37 @@ let gChannels = [];
 client.login(TOKEN);
 
 client.on("ready", () => {
-  console.log("This bot is online");
+    console.log("This bot is online");
 });
 
 client.on("message", async (msg) => {
-  switch (msg.content) {
-    case "MAFIA":
-      try {
-        const server = msg.guild.channels;
-        const townhall = await server.create("Townhall", { type: "voice" });
-        const mafia = await server.create("Mafia", { type: "voice" });
-        gChannels.push(townhall);
-        gChannels.push(mafia);
-      } catch (err) {
-        console.error(err);
-      }
-      break;
-    case "/delete":
-      Promise.all(
-        gChannels.map(async (channel) => {
-          await channel.delete();
-        })
-      ).catch((err) => {
-        console.error(err);
-      });
-      break;
-    case "/guild":
-      console.log(`The guild name is ${msg.guild.name}`);
-    default:
-  }
+    switch (msg.content) {
+        case "MAFIA":
+            try {
+                const server = msg.guild.channels;
+                const townhall = await server.create("Townhall", {
+                    type: "voice",
+                });
+                const mafia = await server.create("Mafia", { type: "voice" });
+                gChannels.push(townhall);
+                gChannels.push(mafia);
+            } catch (err) {
+                console.error(err);
+            }
+            break;
+        case "/delete":
+            Promise.all(
+                gChannels.map(async (channel) => {
+                    await channel.delete();
+                })
+            ).catch((err) => {
+                console.error(err);
+            });
+            break;
+        case "/createGuild":
+			
+        case "/guild":
+            console.log(`The guild name is ${msg.guild.name}`);
+        default:
+    }
 });
